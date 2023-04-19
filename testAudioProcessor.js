fd6fab5df2a45c6d9166d12661e7bee51da6fa4e
gabriele bessi <gabriele.bessi94@gmail.com>
2023-04-20 00:43:01 +0200

    added parameters

--- testAudioProcessor.js
+++ testAudioProcessor.js
@@ -24,5 +24,21 @@ class MyAudioProcessor extends AudioWorkletProcessor {
         }
         return true;
     }
+    static get parameterDescriptors() {
+        return [
+            {
+                name: "gain",
+                defaultValue: 0.5,
+                minValue: 0,
+                maxValue: 1
+            },
+            {
+                name: "frequency",
+                defaultValue: 440.0,
+                minValue: 27.5,
+                maxValue: 4186.009
+            }
+        ];
+    }
 }
 registerProcessor("my-audio-processor", MyAudioProcessor);
